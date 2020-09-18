842d7442cf0c208fadbf6f33215e13492e11166f
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-18 12:31:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,8 @@ def summon_captain_planet(object)
 end
 
 def long_planeteer_calls((planeteer_calls)
-  # Your code here	  planeteer_calls.any? do |call|
   call.length > 4
   end
-	end)
-  # Your code here
 end
 
 def find_the_cheese# code an argument here
